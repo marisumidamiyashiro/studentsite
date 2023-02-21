a637a7aa387c140e0520f24f9b556cb0b4ca8803
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2023-02-21 06:46:27 -1000

    added v1 for digital product

---
+++ art285/themoon-v1/js/script.js
@@ -0,0 +1,19 @@
+
+// SWIPERJS 
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    mousewheel: true,
+    loop: true,
+    effect: 'slide',
+    
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+// SCROLL-TRIGGERED ANIMATIONS
+
+});
+
