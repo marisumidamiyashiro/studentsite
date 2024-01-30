a7ad12f7a2d7ced1db2776f8857af13809f8e830
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2024-01-30 12:00:40 -1000

    fixed js in 0.2 and 0.3; updated 0.3 test

--- art285/portfolio-test-0.2/js/script.js
+++ art285/portfolio-test-0.2/js/script.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const listItems = document.querySelectorAll(".site-nav li");
 
     listItems.forEach((listItem) => {
-        console.log("please work")
         listItem.addEventListener("click", () => {
-            console.log("hello")
             listItems.forEach((otherItem) => {
                 otherItem.classList.remove('active')
             })
